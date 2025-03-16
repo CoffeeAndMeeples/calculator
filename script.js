a42e306119a1c93d69b05fb6b655182822910031
Cody Jorgensen <137858034+CoffeeAndMeeples@users.noreply.github.com>
2025-03-15 20:40:09 -0500

    Create Operate and 4 operator functions

---
+++ script.js
@@ -0,0 +1,43 @@
+let num1;
+let num2;
+let operator;
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+function operate(num1, operator, num2) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+    }
+    return result
+}
+
+console.log(operate(8, "+", 4));
+console.log(operate(8, "-", 4));
+console.log(operate(8, "*", 4));
+console.log(operate(8, "/", 4));
